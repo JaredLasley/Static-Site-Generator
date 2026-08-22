5b9513565c0c4662e4a56105a71bd84bd0cc3dbb
JaredLasley <lasleyjared@gmail.com>
2026-08-21 22:52:13 -0400

    E: Completed text_to_html function along with a few test cases

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 
 class TestTextNode(unittest.TestCase):
@@ -18,7 +18,7 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("Text one is different from text two", TextType.TEXT)
         node2 = TextNode("Text two is different from text one",TextType.TEXT)
         self.assertNotEqual(node,node2)
-        
+
     def test_url(self):
         node = TextNode("Text node test text", TextType.BOLD, url=None)
         node2 = TextNode("Text node test text", TextType.BOLD)
@@ -29,6 +29,17 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("Text node test text", TextType.BOLD)
         self.assertNotEqual(node,node2)
 
+    def test_text_to_html(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_text_to_html_bold(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a text node")
 
 if __name__ == "__main__":
     unittest.main()
